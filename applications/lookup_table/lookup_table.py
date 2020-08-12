0e4305ed27a792c11136c2c05b0f6fa4c29be069
luisabellan <luis.abellan.dev@gmail.com>
2020-08-12 14:47:21 +0200

    refactored look_uptable.py
    made a function create_table instead, so it can be reused and looks cleaner anyway

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -11,27 +11,37 @@ def slowfun_too_slow(x, y):
 
     return v
 
+def create_table():
+    # keys will be arrays holding x and y, where x
+    table = {}
+
+    for x in range(2, 14):
+        for y in range(3, 6):
+            table[x, y] = ((math.factorial(math.pow(x, y))) //
+                        (x + y)) % 982451653 
+
+    return table
+
+table = create_table()
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
-    d = dict()
-    for i in range(2,14):-
-
-    print(d)
+  
+   
+    return table.get((x,y))
+   
     
-    #return ((math.factorial(math.pow(x, y)))//(x+y)) % 982451653
-
 
 
 
 # Do not modify below this line!
 
-""" for i in range(50000):
+for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
-    print(f'{i}: {x},{y}: {slowfun_too_slow(x, y)}') """
+    
     
