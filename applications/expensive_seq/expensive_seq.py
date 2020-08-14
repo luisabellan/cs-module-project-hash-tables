af492713ed8b7e9d8157b9c9990aa467a8e67b8e
luisabellan <luis.abellan.dev@gmail.com>
2020-08-14 18:08:50 +0200

    expensive_seq.py done

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,27 +1,22 @@
 # Your code here
 
 
-def expensive_seq(x, y, z):
-    # Your code here
+expensive_seq_cache = {}
+
+def expensive_seq(x,y,z):
+    input = (x,y,z)
+    if input in expensive_seq_cache:
+        return expensive_seq_cache[(input)]
+   
     if x <= 0:
-        return y + z
-    if x > 0: 
-        return expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        value = y + z
+    elif x >  0:
+        value = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    expensive_seq_cache[input] = value
+    return value
 
 
-def create_table():
-    # keys will be arrays holding x and y, where x
-    table = {}
 
-    for x in range(2, 14):
-        for y in range(3, 6):
-            table[(x, y)] = ((math.factorial(math.pow(x, y))) //
-                             (x + y)) % 982451653
-
-    return table
-
-
-table = create_table()
 
 if __name__ == "__main__":
     for i in range(10):
